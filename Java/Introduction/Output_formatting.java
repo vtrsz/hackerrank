897f58944bfbd1fb7d9033cda829af28aecd3336
vtrsz <konohs@gmail.com>
2022-06-28 20:30:24 -0300

    finished output fortmatting

---
+++ Java/Introduction/Output_formatting.java
@@ -0,0 +1,17 @@
+package Java.Introduction;
+
+import java.util.Scanner;
+
+public class Output_formatting {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++){
+            String str = scanner.next();
+            int x = scanner.nextInt();
+            System.out.printf("%-15s%03d\n", str, x);
+        }
+        scanner.close();
+        System.out.println("================================");
+    }
+}
